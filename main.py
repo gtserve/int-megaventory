83e940a3240560ca3b308dcdfd73f3e2823ab731
George Tservenis <george.tservenis@gmail.com>
2022-03-05 16:09:11 +0200

    SupplierClient Done

--- main.py
+++ main.py
@@ -19,35 +19,48 @@ headers = {"Content-Type": "application/json"}
 class Product:
 
     def __init__(self, sku: str, description: str) -> None:
-        self.product_id = -1
+        self.record_id = -1
         self.sku = sku
         self.description = description
         self.sales_price = 0.00
         self.purchase_price = 0.00
 
-    def mv_product(self) -> dict:
+    def record_dict(self) -> dict:
         return {"ProductSKU": self.sku,
                 "ProductDescription": self.description,
                 "ProductSellingPrice": self.sales_price,
                 "ProductPurchasePrice": self.purchase_price}
 
 
-class Supplier:
+class SCType(Enum):
+    SUPPLIER = 1
+    CLIENT = 2
+    SnC = 3
 
-    def __init__(self, name: str) -> None:
+
+class SupplierClient:
+
+    def __init__(self, name: str, sc_type: SCType) -> None:
+        self.record_id = -1
+        self.sc_type = sc_type
         self.name = name
         self.email = ""
         self.shipping_address = ""
         self.phone = ""
 
+    def record_dict(self) -> dict:
+        if self.sc_type == SCType.SUPPLIER:
+            sc_type_str = "Supplier"
+        elif self.sc_type == SCType.CLIENT:
+            sc_type_str = "Client"
+        else:
+            sc_type_str = "Supplier and Client"
 
-class Client:
-
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.email = ""
-        self.shipping_address = ""
-        self.phone = ""
+        return {"SupplierClientType": sc_type_str,
+                "SupplierClientName": self.name,
+                "SupplierClientEmail": self.email,
+                "SupplierClientShippingAddress1": self.shipping_address,
+                "SupplierClientPhone1": self.phone}
 
 
 class InventoryLocation:
@@ -90,32 +103,61 @@ class Discount:
 #         self.status = status
 
 
-def api_key_verify(key: str) -> str:
+def api_key_verify(key: str) -> int:
     data = {"APIKEY": api_key}
-    response = requests.post(f"{base_url}/APIkey/APIkeyGet", headers=headers, json=data)
-    return response.json()
+
+    response = requests.post(f"{base_url}/APIkey/APIkeyGet",
+                             headers=headers, json=data)
+
+    return int(response.json()["ResponseStatus"]["ErrorCode"])
 
 
-def insert_product(product: Product, ext_app: str) -> str:
+def insert_product(product: Product, ext_app: str) -> int:
     data = {"APIKEY": api_key,
-            "mvProduct": product.mv_product(),
+            "mvProduct": product.record_dict(),
             "mvRecordAction": "Insert"}
 
     if ext_app != "":
         # The ProductUpdate was triggered by an external application.
         data["mvInsertUpdateDeleteSourceApplication"] = ext_app
 
-    response = requests.post(f"{base_url}/Product/ProductUpdate", headers=headers, json=data)
+    response = requests.post(f"{base_url}/Product/ProductUpdate",
+                             headers=headers, json=data)
 
     # Store the ProductID that was assigned.
-    product.product_id = response.json()["mvProduct"]["ProductID"]
+    product.record_id = response.json()["mvProduct"]["ProductID"]
 
-    return response.json()["ResponseStatus"]["ErrorCode"]
+    # Return the error code of the response message.
+    return int(response.json()["ResponseStatus"]["ErrorCode"])
+
+
+def insert_supplier_client(sc: SupplierClient, ext_app: str) -> int:
+    data = {"APIKEY": api_key,
+            "mvSupplierClient": sc.record_dict(),
+            "mvRecordAction": "Insert"}
+
+    if ext_app != "":
+        # The SupplierClientUpdate was triggered by an external application.
+        data["mvInsertUpdateDeleteSourceApplication"] = ext_app
+
+    response = requests.post(f"{base_url}/SupplierClient/SupplierClientUpdate",
+                             headers=headers, json=data)
+
+    # Store the SupplierClientID that was assigned.
+    sc.record_id = response.json()["mvSupplierClient"]["SupplierClientID"]
+
+    # Return the error code of the response message.
+    return int(response.json()["ResponseStatus"]["ErrorCode"])
 
 
 if __name__ == '__main__':
+    supplier = SupplierClient("George_0", SCType.SUPPLIER)
+    supplier.email = "george@mail.com"
+    supplier.shipping_address = "Here 56, Athens"
+    supplier.phone = "6923452345"
 
-    p = Product("PRODUCT_103", "Description")
-    print(p.product_id)
-    print(insert_product(p, ""))
-    print(p.product_id)
+    print(supplier.record_id)
+    print("Error code: ", insert_supplier_client(supplier, ""))
+    print(supplier.record_id)
+
+
